17737677fc28556762e19df614b4f7549fd54125
paichato <tsenane14@gmail.com>
2022-04-15 16:41:44 +0200

    added animations and fixed bugs

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,11 @@
-import { useNavigation } from '@react-navigation/core';
-import React, { useRef, useState } from 'react';
-import { TextInput } from 'react-native';
+import { useNavigation } from "@react-navigation/core";
+import React, { useRef, useState } from "react";
+import { TextInput } from "react-native";
 
-import { Background } from '../../components/Background';
-import { Card } from '../../components/Card';
+import { Background } from "../../components/Background";
+import { Card } from "../../components/Card";
 
-import { useRepositories } from '../../hooks/useRepositories';
+import { useRepositories } from "../../hooks/useRepositories";
 
 import {
   Container,
@@ -15,11 +15,11 @@ import {
   InputField,
   InputButton,
   Icon,
-  RepositoriesList
-} from './styles';
+  RepositoriesList,
+} from "./styles";
 
 export function Dashboard() {
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
   const inputRef = useRef<TextInput>(null);
 
   const { navigate } = useNavigation();
@@ -28,10 +28,12 @@ export function Dashboard() {
 
   function handleAddRepository() {
     /**
-     * TODO: 
+     * TODO:
      * - call addRepository function sending inputText value;
      * - clean inputText value.
      */
+    addRepository(inputText);
+    setInputText("");
     inputRef.current?.blur();
   }
 
@@ -39,18 +41,20 @@ export function Dashboard() {
     /**
      * TODO - navigate to the Repository screen sending repository id.
      * Remember to use the correct prop name (repositoryId) to the repositoy id:
-     * 
+     *
      * navigate(SCREEN NAME, {
      *  repositoryId: id of the repository
      * })
      */
+
+    navigate("Repository", { repositoryId: id });
   }
 
   return (
     <Background>
       <Container>
         <AddGithubRepo>
-          <Title>Explore repositórios{'\n'}no GitHub.</Title>
+          <Title>Explore repositórios{"\n"}no GitHub.</Title>
 
           <Input>
             <InputField
@@ -58,24 +62,26 @@ export function Dashboard() {
               placeholder="Digite aqui 'usuário/repositório'"
               value={inputText}
               /**
-               * TODO - update inputText value when input text value 
+               * TODO - update inputText value when input text value
                * changes:
                * onChangeText={YOUR CODE HERE}
                */
+              onChangeText={setInputText}
               onSubmitEditing={handleAddRepository}
               returnKeyType="send"
-              autoCapitalize='none'
+              autoCapitalize="none"
               autoCorrect={false}
             />
 
             <InputButton
               testID="input-button"
               onPress={handleAddRepository}
-            /**
-             * TODO - ensure to disable button when inputText is 
-             * empty (use disabled prop to this):
-             * disabled={CONDITION HERE}
-             */
+              /**
+               * TODO - ensure to disable button when inputText is
+               * empty (use disabled prop to this):
+               * disabled={CONDITION HERE}
+               */
+              disabled={!!inputText}
             >
               <Icon name="search" size={20} />
             </InputButton>
@@ -85,7 +91,7 @@ export function Dashboard() {
         <RepositoriesList
           data={repositories}
           showsVerticalScrollIndicator={false}
-          keyExtractor={repository => String(repository.id)}
+          keyExtractor={(repository) => String(repository.id)}
           renderItem={({ item: repository }) => (
             <Card
               key={repository.id}
@@ -93,7 +99,7 @@ export function Dashboard() {
                 id: repository.id,
                 title: repository.full_name,
                 subTitle: repository.description,
-                imageUrl: repository.owner.avatar_url
+                imageUrl: repository.owner.avatar_url,
               }}
               onPress={() => handleRepositoryPageNavigation(repository.id)}
             />
@@ -101,5 +107,5 @@ export function Dashboard() {
         />
       </Container>
     </Background>
-  )
+  );
 }
