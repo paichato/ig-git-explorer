526d6430368838011cc628d77f71658032bce4dd
paichato <tsenane14@gmail.com>
2022-04-15 16:40:04 +0200

    added animations and fixed bugs

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { useWindowDimensions, ViewProps } from 'react-native';
+import React, { useEffect } from "react";
+import { useWindowDimensions, ViewProps } from "react-native";
 import {
   useAnimatedStyle,
   useSharedValue,
-  withTiming
-} from 'react-native-reanimated';
+  withTiming,
+} from "react-native-reanimated";
 
-import { AnimationContainer } from './styles';
+import { AnimationContainer } from "./styles";
 
 interface CardAnimationProps extends ViewProps {
   children: React.ReactNode;
@@ -20,19 +20,24 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const animatedStyle = useAnimatedStyle(() => {
     return {
       // TODO - setup animated style
-    }
-  })
+      opacity: cardOpacity.value,
+      transform: [{ translateX: cardOffset.value }],
+    };
+  });
 
   useEffect(() => {
     /**
      * TODO - setup cardOpacity.value and cardOffset.value with
      * withTiming()
      */
+
+    cardOpacity.value = withTiming(1, { duration: 1000 });
+    cardOffset.value = withTiming(0, { duration: 1000 });
   }, []);
 
   return (
     <AnimationContainer {...rest} style={animatedStyle}>
       {children}
     </AnimationContainer>
-  )
+  );
 }
